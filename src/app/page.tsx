c0390afb94d7d5e8c929fc07e4167e9aad3dfca4
henry <henrypearson422@gmail.com>
2024-05-01 16:02:15 +0100

    feat(home): container for welcome message and area for prompt

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,6 @@ import { auth } from "@/app/firebase/config";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 import { Button } from "@/components/ui/button";
-
 export default function Home() {
   const [user] = useAuthState(auth);
   const router = useRouter();
@@ -19,17 +18,20 @@ export default function Home() {
   console.log({ user });
   return (
     <main>
-      Hello {user?.email}
-      <Button
-        onClick={() => {
-          auth.signOut();
-          router.push("/sign-in");
-          sessionStorage.removeItem("user");
-        }}
-        className="block mx-auto mb-4 border w-1/2"
-      >
-        Log out
-      </Button>
+      <div className="font-extrabold text-6xl text-center m-10 glow">Prompt goes here.</div>
+      <div className="w-2/3 text-center bg-card text-card-foreground border mx-auto p-5 m-5 rounded-lg">
+        <p className="mb-4">Hello {user?.email}!</p>
+        <Button
+          onClick={() => {
+            auth.signOut();
+            router.push("/sign-in");
+            sessionStorage.removeItem("user");
+          }}
+          className="block mx-auto mb-4 border w-1/6"
+        >
+          Log out
+        </Button>
+      </div>
     </main>
   );
 }
