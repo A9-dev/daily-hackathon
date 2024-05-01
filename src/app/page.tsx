26394395a3db5b95a4a1e605573706602856327f
henry <henrypearson422@gmail.com>
2024-05-01 15:34:02 +0100

    ok fixed stuff
     (I am now going to start using conventional commits)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,35 @@
-import Image from "next/image";
+"use client";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "@/app/firebase/config";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
-  return <main>Test</main>;
+  const [user] = useAuthState(auth);
+  const router = useRouter();
+
+  const userSession = sessionStorage.getItem("user");
+  useEffect(() => {
+    if (!user && !userSession) {
+      router.push("/sign-in");
+    }
+  }, [user, router, userSession]);
+
+  console.log({ user });
+  return (
+    <main>
+      Hello {user?.email}
+      <Button
+        onClick={() => {
+          auth.signOut();
+          router.push("/sign-in");
+          sessionStorage.removeItem("user");
+        }}
+        className="block mx-auto mb-4 border w-1/2"
+      >
+        Log out
+      </Button>
+    </main>
+  );
 }
--- src/app/sign-in/page.tsx
+++ src/app/sign-in/page.tsx
@@ -2,24 +2,25 @@
 import { useState } from "react";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
-
-import { useCreateUserWithEmailAndPassword } from "react-firebase-hooks/auth";
+import { useSignInWithEmailAndPassword } from "react-firebase-hooks/auth";
 import { auth } from "@/app/firebase/config";
 import { useRouter } from "next/navigation";
 
-export default function SignUpPage() {
+export default function SignInPage() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [createUserWithEmailAndPassword] = useCreateUserWithEmailAndPassword(auth);
+  const [signInWithEmailAndPassword] = useSignInWithEmailAndPassword(auth);
   const router = useRouter();
 
   async function handleSubmit(event: React.FormEvent<HTMLButtonElement>) {
     event.preventDefault();
     try {
-      const res = await createUserWithEmailAndPassword(email, password);
+      const res = await signInWithEmailAndPassword(email, password);
       setEmail("");
       setPassword("");
+      sessionStorage.setItem("user", JSON.stringify(res?.user));
       console.log({ res });
+      router.push("/");
     } catch (error: any) {
       console.error(error.message);
     }
@@ -28,7 +29,7 @@ export default function SignUpPage() {
   return (
     <div className="w-1/3 p-5 mx-auto rounded my-5 border ">
       <h1 className="scroll-m-20 mb-5 text-4xl font-extrabold tracking-tight lg:text-5xl">
-        Sign Up
+        Sign In
       </h1>
       <Input
         type="email"
@@ -48,15 +49,15 @@ export default function SignUpPage() {
         required
         className="w-full mb-4 p-2 border rounded"
       />
-      <Button onClick={handleSubmit} className="block mb-4 mx-auto border w-1/2">
-        Sign Up
+      <Button onClick={handleSubmit} className="block mx-auto mb-4 border w-1/2">
+        Sign In
       </Button>
 
       <Button
-        onClick={() => router.push("/sign-in")}
+        onClick={() => router.push("/sign-up")}
         className="block bg-secondary text-secondary-foreground  mx-auto border w-1/2"
       >
-        Already have an account? Sign in.
+        Not a user? Sign up.
       </Button>
     </div>
   );
