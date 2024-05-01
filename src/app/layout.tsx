9d592c72b780541fffd6a4dd3be3af4874559e5a
henry <henrypearson422@gmail.com>
2024-05-01 15:49:42 +0100

    fix(sign-in): invalid sign-in causes error
    Also added an error alert to display the error.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({
         <body>
           <ThemeProvider
             attribute="class"
-            defaultTheme="system"
+            defaultTheme="dark"
             enableSystem
             disableTransitionOnChange
           >
--- src/app/sign-in/page.tsx
+++ src/app/sign-in/page.tsx
@@ -6,20 +6,28 @@ import { useSignInWithEmailAndPassword } from "react-firebase-hooks/auth";
 import { auth } from "@/app/firebase/config";
 import { useRouter } from "next/navigation";
 
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+
 export default function SignInPage() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [signInWithEmailAndPassword] = useSignInWithEmailAndPassword(auth);
   const router = useRouter();
+  const [error, setError] = useState("");
 
   async function handleSubmit(event: React.FormEvent<HTMLButtonElement>) {
     event.preventDefault();
     try {
       const res = await signInWithEmailAndPassword(email, password);
+      if (!res) {
+        console.log("User not found");
+        setError("Email and password do not match. Please try again.");
+        return;
+      }
       setEmail("");
       setPassword("");
+      setError("");
       sessionStorage.setItem("user", JSON.stringify(res?.user));
-      console.log({ res });
       router.push("/");
     } catch (error: any) {
       console.error(error.message);
@@ -27,10 +35,16 @@ export default function SignInPage() {
   }
 
   return (
-    <div className="w-1/3 p-5 mx-auto rounded my-5 border ">
+    <div className="w-1/3 p-5 mx-auto rounded my-5 border">
       <h1 className="scroll-m-20 mb-5 text-4xl font-extrabold tracking-tight lg:text-5xl">
         Sign In
       </h1>
+      {error && (
+        <Alert variant="destructive" className="mb-4">
+          <AlertTitle>Error</AlertTitle>
+          <AlertDescription>{error}</AlertDescription>
+        </Alert>
+      )}
       <Input
         type="email"
         name="email"
